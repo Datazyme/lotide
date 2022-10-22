762674d77a7ca53f3729b816e41709c9228d35fa
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-10-22 21:33:59 +0000

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🍏 Assertion Passed:" + actual + " === " + expected);
+  } else {
+    console.log("🍎 Assertion Failed:" + actual + " !== " + expected);
+  }
+};
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Moon", "Gear");
+assertEqual("Moon", "Moon");
+assertEqual(5, 9);
