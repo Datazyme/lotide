6da55e1cd2d3873cc56271231486b75259ab0856
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-10-25 06:24:22 +0000

    works again

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,12 @@
 function eqArrays(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
-    } else {
+    if (array1[i] !== array2[i]) {
       return false;
     }
-    
   }
+  return true;
 }
+
 const assertEqual = function(eqArrays, expected) {
   if (eqArrays === expected) {
     console.log(`🍏 Assertion Passed: ${eqArrays} === ${expected}`);
@@ -16,9 +15,9 @@ const assertEqual = function(eqArrays, expected) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3,], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
