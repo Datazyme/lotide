d5821392798f173b6730f85a5af787b094a40221
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-10-25 01:51:23 +0000

    asserting if arrays are the same

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertEqual = function(eqArrays, expected) {
+  if (eqArrays === expected) {
+    console.log(`🍏 Assertion Passed: ${eqArrays} === ${expected}`);
+  } else {
+    console.log(`🍎 Assertion Failed: ${eqArrays} === ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3,], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
