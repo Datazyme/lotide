a9bcfcdaa0799a780b87e7bc4494b77f4a4dd2a7
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-11 23:12:21 +0000

    assertEqual test instead of inside js file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+
+//Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Moon", "Gear");
+assertEqual("Moon", "Moon");
+assertEqual(5, 9);
