c55859923835cc82d275f21ec265fceecb2fa4bd
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-11 23:25:36 +0000

    tests moved to test folder

--- head.js
+++ head.js
@@ -1,17 +1,14 @@
-const head = function (actual) {
- let y = actual[0];
- return y;
+//const assertEqual = require('./assertEqual');
+
+const head = function(actual) {
+  let y = actual[0];
+  return y;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🍏 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
+/*Test code
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1,6,7]), 5);
 assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+assertEqual(head([]), 5);*/
+module.exports = head;
