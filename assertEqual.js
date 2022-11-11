15fc4b148e8c76772526ed4fbc94c662ec9130de
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-11 23:23:58 +0000

    tests removed to test folder

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,15 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🍏 Assertion Passed:" + actual + " === " + expected);
+    console.log(`🍏 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🍎 Assertion Failed:" + actual + " !== " + expected);
+    console.log(`🍎 Assertion Failed: ${actual} === ${expected}`);
   }
 };
 
-//Test code
+/*Test code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1,1);
 assertEqual("Moon", "Gear");
 assertEqual("Moon", "Moon");
-assertEqual(5, 9);
+assertEqual(5, 9);*/
+module.exports = assertEqual;
