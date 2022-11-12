f0da56ddad5f7916fb596a9dd31ed4e23413e293
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-12 08:05:02 +0000

    refractored to use with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,28 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 const middle = require('../middle');
+const assert = (require('chai').assert);
 
+describe("#tail", () => {
+  it("returns empty array when length is one", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns empty array when length is 2", () => {
+    assert.deepEqual(middle([1, 2]), []); 
+  });
+  it("returns middle value of input array has odd length", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+  it("returns 2 middle values of input array has even length", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+});
 
-assertEqual((middle([1])), true) // => []
-assertEqual((middle([1, 2])), true) // => []
-assertEqual(middle([1, 2, 3]), true)
+/*
+assertEqual(middle([1]), []) // => []
+assertEqual(middle([1, 2]), []) // => []
+assertEqual(middle([1, 2, 3]), "2")
 
 console.log(middle([1, 2, 3])) // => [2]
 console.log(middle([1, 2, 3, 4, 5])) // => [3]
 console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]*/
