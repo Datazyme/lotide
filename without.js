72ea31fb06e2b0ea7e5e67734d28739379c4e0f5
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-13 23:53:54 +0000

    removing unwanted items from array

---
+++ without.js
@@ -0,0 +1,24 @@
+
+const without = function(fullArr, unwantedItem) {
+  let cleanArr = [];
+  for (const item of fullArr) {
+    if (!unwantedItem.includes(item)) {
+      cleanArr.push(item);
+    }
+  }
+  return cleanArr;
+};
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+
+//const assertEqual = function(eqArrays, expected) {
+  //if (eqArrays === expected) {
+    //console.log(`🍏 Assertion Passed: ${eqArrays} === ${expected}`);
+  //} else {
+    //console.log(`🍎 Assertion Failed: ${eqArrays} === ${expected}`);
+  //}
+//};
