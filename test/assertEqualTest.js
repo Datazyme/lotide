5bedd92c93aca7a0b87f54721f1289bc5781736c
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-17 07:13:11 +0000

    with mocha and chai tests and comments

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,25 @@
-const assertEqual = require('../assertEqual')
+//imports assertEqual
+const assertEqual = require('../assertEqual');
+//imports chai
+const assert = (require('chai').assert);
 
-//Test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Moon", "Gear");
-assertEqual("Moon", "Moon");
-assertEqual(5, 9);
+//mocha gives describe and it functions, 
+//must be run with "npm test folder/filename" in terminal
+describe("#assertEqual", () => {
+  it("returns false when comparing Lighthouse Labs and Bootcamp", () => {
+    assert.strictEqual(assertEqual("Lighthouse Labs", "Bootcamp"));
+  });
+  it("returns true when comparing 1 and 1", () => {
+    assert.strictEqual(assertEqual(1,1));
+  });
+  it("returns false when comparing Moon and Gear", () => {
+    assert.strictEqual(assertEqual("Moon", "Gear"));
+  });
+  it("returns true when comparing Moon and Moon", () => {
+    assert.strictEqual(assertEqual("Moon", "Moon"));
+  });
+  it("returns false when comparing 5 and 9", () => {
+    assert.strictEqual(assertEqual(5, 9));
+  });
+
+});
