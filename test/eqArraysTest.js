27ad9295ee8094bf2bb40fcf5fee6bf761f66849
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-17 08:31:12 +0000

    with comments and mocha and chai tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,27 @@
-const assertEqual = require('../assertEqual');
+//imports eqArrays
 const eqArrays = require('../eqArrays')
 
-assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3,], [3, 2, 1]), false); // => false
+//imports chai
+const assert = (require('chai').assert);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+//mocha uses describe and it
+describe("#eqArrays", () => {
+  it("returns true when arrays are equal", () => {
+    assert.deepEqual(eqArrays([1, 2, 3,], [1, 2, 3]), true);
+  });
+  it("returns false when arrays are not equal", () => {
+    assert.deepEqual(eqArrays([1, 2, 3,], [3, 2, 1]), false);; 
+  });
+  it("returns true if values are equal strings", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+  });
+  it("returns false if the values in the array are not strictly equal", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
+  });
+  it("returns false if one array is empty", () => {
+    assert.deepEqual(eqArrays([], [3, 5]), false); 
+  });
+  it("returns false if one array is empty", () => {
+    assert.deepEqual(eqArrays(["1", 2], []), false); 
+  });
+});
