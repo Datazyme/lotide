6026d16df1046da3c05b6b35444f88e7599e91a6
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-17 07:14:35 +0000

    tests removed and comments added, exported to test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,9 @@
+//function determines if inputs are equal by strict comparison and returns truthy value
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🍏 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🍎 Assertion Failed: ${actual} === ${expected}`);
+    console.log(`🍎 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-/*Test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Moon", "Gear");
-assertEqual("Moon", "Moon");
-assertEqual(5, 9);*/
 module.exports = assertEqual;
