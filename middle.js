b7e5e34e379fb610f9a2ba277db8e97f2374c214
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-11-18 02:24:14 +0000

    now with comments and cleaned up

--- middle.js
+++ middle.js
@@ -1,26 +1,22 @@
+//function finds the middle number in an array and returns it if the array is odd
+//if the array is even it returns the two middle values
 const middle = function(array) {
-  let midArr = [];    
-    if (array.length <= 2) {
-      return midArr;
-    };
-    //return middle value of odd length array
-    if (array.length % 2 !== 0) {
-      let midVal = Math.floor((array.length - 1) / 2);
-      midArr.push(array[midVal]);
-    } else if(array.length % 2 === 0) {
-      let midValLess = Math.floor((array.length - 1) / 2)
-      midArr.push(array[midValLess])
-      let midValMore = array.length / 2;
-      midArr.push(array[midValMore])
-      
-    };
+  let midArr = [];
+  //returns empty array if the input array has two or less values
+  if (array.length <= 2) {
+    return midArr;
+  };
+  //return middle value of odd length array
+  if (array.length % 2 !== 0) {
+    let midVal = Math.floor((array.length - 1) / 2);
+    midArr.push(array[midVal]);
+  //returns two middle values for even array length
+  } else if(array.length % 2 === 0) {
+    let midValLess = Math.floor((array.length - 1) / 2);
+    midArr.push(array[midValLess]);
+    let midValMore = array.length / 2;
+    midArr.push(array[midValMore]);   
+  };
   return midArr;
-}
-
-//Tests
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+};
+module.exports = middle;
