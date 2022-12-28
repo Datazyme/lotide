82c556725084c61cd967245936585899131b5be9
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-28 22:57:26 +0000

    with module export and comments

--- takeUntil.js
+++ takeUntil.js
@@ -1,23 +1,14 @@
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+//function that takes the first portion of an input array to a point specified by an input callback
 const takeUntil = function(array, callback) {
-  //console.log(callback);
   let newArr = [];
   for (let item in array) {
-    //console.log()
+    //callback performed on each value in array
     let value = callback(array[item]);
-    if (value === true) {
-      
-      newArr.push(array.slice(0, item))
+    if (value === true) {  
+      //pushes values passing callback into new array    
+      newArr.push(array.slice(0, item));
     }
   }
   return newArr;
-}
-
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+};
+module.exports = takeUntil;
