ced97b3debd4eb70647a7ecaf89fdb9cf59c9e92
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-29 06:07:38 +0000

    test for findKeyByValue

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,26 @@
+//import the functions to test
+const findKeyByValue = require('../findKeyByValue');
+const assertEqual = require('../assertEqual');
+const assert = (require('chai').assert);
+
+//mocha gives describe and it functions, 
+//must be run with "npm test folder/filename" in terminal
+describe("#findKeyByValue", () => {
+  it("returns genre of show if show is in object as expected", () => {
+    const bestTVShowsByGenre = { 
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+    assert.strictEqual(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+  });
+
+  it("returns undefined if the show is not in object", () => {
+    const bestTVShowsByGenre = { 
+      sci_fi: "The Expanse",
+      comedy: "Brooklyn Nine-Nine",
+      drama:  "The Wire"
+    };
+    assert.strictEqual(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
+  });
+});
