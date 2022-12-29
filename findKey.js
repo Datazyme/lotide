d7512ad64db4cb0678bb589a114926fa98299f75
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-29 07:05:28 +0000

    with comments

--- findKey.js
+++ findKey.js
@@ -1,16 +1,15 @@
+//function loops through elements (keys) in object, compares values to values in callback 
+//function and returns key if there is a match
 const findKey = (obj, callback) => {
   for(const element in obj) {
-    //element = key of object ie "Blue Hill"
-    //console.log(element); 
     if (callback(obj[element])) {
       return element;
     }
   }
-
-}
+};
 
 const key = findKey({
-  "Blmue Hill": { stars: 1 },
+  "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
@@ -18,4 +17,4 @@ const key = findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2) // => "noma"
 
-console.log(key)
+module.exports = findKey;
