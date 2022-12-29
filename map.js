5081a2f29f057ff46915265d45aacaccf7944025
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-29 08:27:27 +0000

    with comments and export

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+//function to test if arrays are equal
 function eqArrays(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -6,8 +7,9 @@ function eqArrays(array1, array2) {
   }
   return true;
 }
+
+//test for eqArrays function
 const assertEqual = function(eqArrays, expected) {
-  //console.log(expected);
   if (eqArrays === expected) {
     console.log(`🍏 Assertion Passed: ${eqArrays} === ${expected}`);
   } else {
@@ -16,19 +18,14 @@ const assertEqual = function(eqArrays, expected) {
 };
 
 const words = ["ground", "control", "to", "major", "tom"];
+
+//takes in array and callback function to return new, transformed array
 const map = function(array, callback) {
-  // temporary code
-  //console.log("array: ", array);
-  //console.log("callback: ", callback);
   const results = [];
   for(let item of array) {
-    //console.log(`item Before: `, item);
-    //console.log(`item After: `, callback(item));
-    //console.log('---')
     results.push(callback(item));
   }
   return results;
 }
 
-const results1 = map(words, word => word[0]);
-console.log(assertEqual(eqArrays(results1, words), true));
+module.exports = map;
