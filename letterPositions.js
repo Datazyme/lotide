4f6240347fb9e6170b2992cee7bad74c33fd74ff
Anastasia Kuzmin <anakuzmin2@gmail.com>
2022-12-30 03:44:40 +0000

    with comments and export

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,7 @@
+//function to turn a string into an object that shows each letter and the index at which it occurs
 let countLetters = function(letter) {
   let results = {};
-  
-
+  //loops through string and creates object letter key and index value, spaces are omitted
   for (let i = 0; i < letter.length; i++) {
     //sets key
     let key = letter[i];
@@ -13,18 +13,7 @@ let countLetters = function(letter) {
       }
     }
   } 
-  return results;  
+  return results;
 }
-console.log(countLetters("lighthouse in the house"));
-/*{ 
-  l: [0],
-  i: [1, 11],
-  g: [2],
-  h: [3, 5, 15, 18],
-  t: [4, 14],
-  o: [6, 19],
-  u: [7, 20],
-  s: [8, 21],
-  e: [9, 16, 22],
-  n: [12]
-}*/
+
+module.exports = countLetters;
