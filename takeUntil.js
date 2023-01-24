981bc39c90d259b7c0bb6a661edef59a3befc6bb
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-01-24 05:51:46 +0000

    returns flattened array

--- takeUntil.js
+++ takeUntil.js
@@ -1,14 +1,14 @@
 //function that takes the first portion of an input array to a point specified by an input callback
 const takeUntil = function(array, callback) {
   let newArr = [];
-  for (let item in array) {
+  for (const item in array) {
     //callback performed on each value in array
     let value = callback(array[item]);
-    if (value === true) {  
+    if (value) {  
       //pushes values passing callback into new array    
       newArr.push(array.slice(0, item));
     }
   }
-  return newArr;
+  return newArr.flat();
 };
 module.exports = takeUntil;
