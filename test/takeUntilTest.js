c50427a4f23a8c8829592da34aeb6ba1f8d1c7cf
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-01-24 05:44:23 +0000

    arrays not nested

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -8,12 +8,12 @@ describe("#takeUntil", () => {
   it("returns all numbers that are before a number that is less than zero", () => {
     const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
     const results1 = takeUntil(data1, x => x < 0);
-    assert.deepEqual(results1, [[1, 2, 5, 7, 2]]);
+    assert.deepEqual(results1, [1, 2, 5, 7, 2]);
   });
   it("returns everything before the comma", () => {
     const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
     const results2 = takeUntil(data2, x => x === ',');
-    assert.deepEqual(results2, [["I've", "been", "to", "Hollywood"]]); 
+    assert.deepEqual(results2, ["I've", "been", "to", "Hollywood"]); 
   });
 
   it("returns an empty array if input array is empty", () => {
