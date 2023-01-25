82c2d179413402b326b570f8c29650b5a18a5e63
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-01-25 06:35:12 +0000

    imports assertObjectsEqual

--- test/assertEqObjectsTest.js
+++ test/assertEqObjectsTest.js
@@ -1,27 +1,9 @@
-const eqObjects = require('../eqObjects');
-const assert = (require('chai').assert);
+const assertObjectsEqual = require('../assertObjectsEqual');
 
-//mocha gives describe and it functions, 
-//must be run with "npm test folder/filename" in terminal
-describe("#eqObjects", () => {
-  it("returns true if keys and values are the same", () => {
-    const ab = { a: "1", b: "2" };
-    const ba = { b: "2", a: "1" };
-    assert.deepEqual(eqObjects(ab, ba), true);
-  });
-  it("returns false if some but not all key/value pairs match", () => {
-    const ab = { a: "1", b: "2" };
-    const abc = { a: "1", b: "2", c: "3" };
-    assert.deepEqual(eqObjects(ab, abc), false); 
-  });
-  it("returns true if key/value pairs match and values are arrays that match", () => {
-    const cd = { c: "1", d: ["2", 3] };
-    const dc = { d: ["2", 3], c: "1" };
-    assert.deepEqual(eqObjects(cd, dc), true); 
-  });
-  it("returns false if keys match but values are arrays that don't match", () => {
-    const cd2 = { c: "1", d: ["2", 3, 4] };
-    const cd = { c: "1", d: ["2", 3] };
-    assert.deepEqual(eqObjects(cd, cd2), false); 
-  });
-});
+assertObjectsEqual({ a: 1, b: 2, c: 3}, { a: 1, b: 2, c: 3});
+assertObjectsEqual({ a: "1", b: "2" }, { b: "2", a: "1"});
+assertObjectsEqual({ a: 1, b: 2, c: 3}, { a: 1, b: 2});
+assertObjectsEqual({ a: 1, b: 2, c: 3}, { a: 1, b: 2, c: '3'});
+assertObjectsEqual({ a: "1", b: "2", c: "3"}, { a: "1", b: "2"});
+assertObjectsEqual({ c: "1", d: ["2", 3]}, { d: ["2", 3], c: "1"});
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4, 8]});
