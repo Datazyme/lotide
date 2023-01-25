c26a16e0482b54dadd8ed8b784765af1f7c0c380
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-01-25 07:07:23 +0000

    without assertEqual

--- test/headTest.js
+++ test/headTest.js
@@ -1,27 +1,24 @@
 //import assertEqual to get truthy statements
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const head   = require('../head');
 const assert = (require('chai').assert);
 
 //mocha gives describe and it functions, 
 //must be run with "npm test folder/filename" in terminal
 describe("#head", () => {
   it("returns true for 1 and [1, 2, 3]", () => {
-    assert.strictEqual(assertEqual(head([1, 2, 3]), 1)), true;
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns true for '5' and ['5']", () => {
-    assert.strictEqual(assertEqual(head(['5']), '5')), true; 
+    assert.strictEqual(head(['5']), '5'); 
   });
   it("returns '5' for [5, 6, 7]", () => {
-    assert.strictEqual(assertEqual(head([5, 6, 7]), 5)), true; 
+    assert.strictEqual(head([5, 6, 7]), 5); 
   });
   it("returns 'Hello' for [Hello, Lighthouse, Labs]", () => {
-    assert.strictEqual(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")), true; 
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
   });
-  it("returns false for [1, 6, 7] and 5", () => {
-    assert.strictEqual(assertEqual(head([1, 6, 7]), 5)), false;
-  });
-  it("returns false for [] and 5", () => {
-    assert.strictEqual(assertEqual(head([]), 5)), false;
-  });
+  
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  })
 });
