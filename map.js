4bb2d403a1157d0e6bc816428d55e47eb1b82f7f
Anastasia Kuzmin <anakuzmin2@gmail.com>
2023-01-25 07:25:41 +0000

    removing unused code

--- map.js
+++ map.js
@@ -1,23 +1,3 @@
-//function to test if arrays are equal
-function eqArrays(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-//test for eqArrays function
-const assertEqual = function(eqArrays, expected) {
-  if (eqArrays === expected) {
-    console.log(`🍏 Assertion Passed: ${eqArrays} === ${expected}`);
-  } else {
-    console.log(`🍎 Assertion Failed: ${eqArrays} === ${expected}`);
-  }
-};
-
-const words = ["ground", "control", "to", "major", "tom"];
 
 //takes in array and callback function to return new, transformed array
 const map = function(array, callback) {
